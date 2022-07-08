72a83ecbc4fb5a4dee97813faf1cf8dc00c329ba
Michael Piotrowski <mxp@dynalabs.de>
2022-07-08 17:12:22 +0200

    Define format of Emacs time stamps.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,3 @@
-((markdown-mode . ((bibtex-completion-bibliography . "bib/references.bib"))))
+((markdown-mode . ((bibtex-completion-bibliography . "bib/references.bib")
+                   (time-stamp-format . "%:y-%02m-%02dT%02H:%02M:%02S%5z %u")
+                   (eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
